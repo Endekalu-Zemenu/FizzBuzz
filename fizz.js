5fba9132959627a7a095e784819c6547a78daf26
Endekalu Zemenu <155809658+Endekalu-Zemenu@users.noreply.github.com>
2024-02-05 10:37:25 +0300

    update

--- fizz.js
+++ fizz.js
@@ -3,16 +3,16 @@ var count = 1;
 
 function fizzBuzz(){
     if(count % 3 === 0 && count % 5 === 0){
-        console.log("FizzBuzz");
+        output.push("fizzBuzz");
     }
-    else if(count % 3 === 0){
-        console.log("Fizz");
+    else if(count % 3 === 0 && count % 5 !== 0){
+        output.push("Fizz");
     }
-    else if(cont % 5 === 0){
-        console.log("Buzz");
+    else if(count % 5 === 0 && count % 3 !== 0){
+        output.push("Buzz");
     }
     else{
-        console.log(output.push(count));
+        output.push(count);
     }
     count++;
     console.log(output);
